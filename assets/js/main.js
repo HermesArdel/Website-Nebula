9b587b54a98faa602a83b64ad63a56e3ba2ac816
Hermes <109424772+HermesArdel@users.noreply.github.com>
2024-06-25 05:28:16 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW =====*/ 
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,7 +11,7 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*===== REMOVE MENU MOBILE =====*/
+
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
@@ -20,7 +20,7 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+
 const sections = document.querySelectorAll('section[id]')
 
 window.addEventListener('scroll', scrollActive)
@@ -41,7 +41,7 @@ function scrollActive(){
     })
 }
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -49,27 +49,26 @@ const sr = ScrollReveal({
     reset: true
 })
 
-/*SCROLL HOME*/
 sr.reveal('.home__title', {})
 sr.reveal('.home__scroll', {delay: 200})
 sr.reveal('.home__img', {origin:'right', delay: 400})
 
-/*SCROLL ABOUT*/
+
 sr.reveal('.about__img', {delay: 500})
 sr.reveal('.about__subtitle', {delay: 300})
 sr.reveal('.about__profession', {delay: 400})
 sr.reveal('.about__text', {delay: 500})
 sr.reveal('.about__social-icon', {delay: 600, interval: 200})
 
-/*SCROLL SKILLS*/
+
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
 sr.reveal('.skills__img', {delay: 400})
 
-/*SCROLL PORTFOLIO*/
+
 sr.reveal('.portfolio__img', {interval: 200})
 
-/*SCROLL CONTACT*/
+
 sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
 sr.reveal('.contact__input', {delay: 400})
